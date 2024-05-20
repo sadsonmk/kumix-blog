8d3a4735116ff337513e3c03256343b2cf7d874d
iamsadson <sadiemcmirror@gmail.com>
2024-05-20 17:02:31 +0800

    created the .env file and the routes folder to handle the blog routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-
+const blogsRoutes = require('./routes/blogs');
 // create an express app
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+app.use('/blogs', blogsRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
