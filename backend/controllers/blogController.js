ebbd8d96a1a68008ffefcf552a5de981d4147d00
iamsadson <sadiemcmirror@gmail.com>
2024-05-20 23:02:19 +0800

    created the controllers module to handle the callback functions for each route, they are called blogControllers

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const Blog = require('../models/blogModel');
+
+// GET all blogs
+const getAllBlogs = async (req, res) => {
+    try{
+        const blogs = await Blog.find({}).sort({ createdAt: -1 });
+        res.status(200).json(blogs);
+    }catch(err){
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// GET a specific blog
+const getBlog = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+    return res.status(200).json(blog);
+};
+
+// POST a new blog
+const createBlog = async (req, res) => {
+    const {title, author, content} = req.body;
+    try{
+       const blog = await Blog.create({title, author, content});
+       res.status(200).json(blog);
+    }catch(err){
+        res.status(400).json({ error: err.message });
+    }
+};
+
+
+// PATCH updated data to a specific blog
+const updateBlog = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+    const blog = await Blog.findByIdAndUpdate(id, { ...req.body });
+    if (!blog) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+    return res.status(200).json({ message: 'Blog updated successfully' });
+}
+
+// DELETE a specific blog
+const deleteBlog = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+
+    const blog = await Blog.findByIdAndDelete(id);
+    if (!blog) {
+        return res.status(404).json({ error: 'Blog not found' });
+    }
+    return res.status(200).json({ message: 'Blog deleted successfully' });
+};
+
+module.exports = {
+    createBlog,
+    getAllBlogs,
+    getBlog,
+    deleteBlog,
+    updateBlog,
+};
--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -1,38 +1,29 @@
 const express = require('express');
-const Blog = require('../models/blogModel');
+const {
+    createBlog,
+    getAllBlogs,
+    getBlog,
+    updateBlog,
+    deleteBlog
+} = require('../controllers/blogController');
+
 
 const router = express.Router();
 
 // GET all blogs
-router.get('/', (req, res) => {
-    res.json({message: 'GET all blogs'});
-})
+router.get('/', getAllBlogs)
 
 // GET a specific blog
-router.get('/:id', (req, res) => {
-    res.json({message: 'GET a specific blog'});
-})
+router.get('/:id', getBlog)
 
 // POST a new blog
-router.post('/', async (req, res) => {
-    const {title, author, content} = req.body;
-    try{
-       const blog = await Blog.create({title, author, content});
-       res.status(200).json(blog);
-    }catch(err){
-        res.status(400).json({ error: err.message });
-    }
-})
+router.post('/', createBlog)
 
 // PATCH updated data to a specific blog
-router.patch('/:id', (req, res) => {
-    res.json({message: 'PATCH updated data to a specific blog'});
-})
+router.patch('/:id', updateBlog)
 
 // DELETE a specific blog
-router.delete('/:id', (req, res) => {
-    res.json({message: 'DELETE a specific blog'});
-})
+router.delete('/:id', deleteBlog)
 
 
 module.exports = router;
