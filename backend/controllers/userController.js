9b2db4c01bbb966660852ff6b99171c31ef3cb15
iamsadson <sadiemcmirror@gmail.com>
2024-05-23 23:50:05 +0800

    created the backend user login and signup using the model and controller, as well as the routing

---
+++ backend/controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// generate token
+const generateToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWT_SECRET, {
+        expiresIn: '3d'
+    });
+};
+// login user
+
+const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        const token = generateToken(user._id);
+        res.status(200).json({ email, token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+
+};
+
+// signup user
+const userSignup = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.signup(email, password);
+        const token = generateToken(user._id);
+        res.status(201).json({ email, token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+module.exports = { userLogin, userSignup };
---
+++ backend/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { userSignup, userLogin } = require('../controllers/userController');
+const router = express.Router();
+
+router.post('/signup', userSignup);
+
+router.post('/login', userLogin);
+
+
+module.exports = router;
