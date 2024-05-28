1f5e0745c44d00036ac0db0f84b40d4d94043edc
iamsadson <sadiemcmirror@gmail.com>
2024-05-28 19:35:07 +0800

    made the created at dynamic using a library date-fns to calculate the time that has passed from the creation of the blog

--- frontend/src/components/BlogDetails.js
+++ frontend/src/components/BlogDetails.js
@@ -9,40 +9,19 @@ import {
   MDBBtn
 } from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom'
-
-// import { useBlogsContext } from '../hooks/useBlogsContext'
-// import { useAuthContext } from '../hooks/useAuthContext'
+import { differenceInDays, differenceInHours, differenceInMinutes } from 'date-fns'
 
 const BlogDetails = ({ blog }) => {
-  // const { dispatch } = useBlogsContext()
-  // const { user } = useAuthContext()
 
-  // const handleClick = async () => {
-
-  //   const res = await fetch('/blogs/' + blog._id, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'Authorization': `Bearer ${user.token}`
-  //     }
-  //   })
-  //   const data = await res.json()
-  //   if (res.ok) {
-  //     dispatch({ type: 'DELETE_BLOG', payload: data })
-  //   }
-  // }
-
+  const createdAt = new Date(blog.createdAt)
+  const daysAgo = differenceInDays(new Date(), createdAt)
+  const hoursAgo = differenceInHours(new Date(), createdAt)
+  const minutesAgo = differenceInMinutes(new Date(), createdAt)
 
   return (
-    // <div className='blog-details'>
-    //     <h3>{blog.title}</h3>
-    //     <p><strong>tip: </strong>{blog.content}</p>
-    //     <p><strong>author: </strong>{blog.author}</p>
-    //     <p>{blog.createdAt}</p>
-    //     <span onClick={handleClick}>delete</span>
-    // </div>
     <div className='blog-details'>
     <MDBCard alignment='center'>
-      <MDBCardHeader>Tip</MDBCardHeader>
+      <MDBCardHeader>Facts and Opinions</MDBCardHeader>
       <MDBCardBody>
         <MDBCardTitle>{blog.title}</MDBCardTitle>
         <MDBCardText>{blog.content.substring(0, 50)}...</MDBCardText>
@@ -52,7 +31,11 @@ const BlogDetails = ({ blog }) => {
           <MDBBtn>Read more...</MDBBtn>
         </Link>
       </MDBCardBody>
-      <MDBCardFooter className='text-muted'>2 days ago</MDBCardFooter>
+      <MDBCardFooter className='text-muted'>
+      {minutesAgo < 60 ? `${minutesAgo} minutes ago` :
+     hoursAgo < 24 ? `${hoursAgo} hours ago` :
+     `${daysAgo} days ago`}
+      </MDBCardFooter>
     </MDBCard>
     </div>
   )
